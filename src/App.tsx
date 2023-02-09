c9a1d652de4604a1746deed3a36ea7f744c861b9
Mayur B Patel <mayurpatel98765@gmail.com>
2023-02-09 13:21:50 +0530

    Add : gtm added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
 import "./App.css";
 
+import TagManager from "react-gtm-module";
+
+TagManager.initialize({ gtmId: "GTM-NKM5DRZ" });
+
 function App() {
+  const clickHandle1 = () => {
+    TagManager.dataLayer({
+      dataLayer: {
+        event: "click_custom_button_1",
+        category: "button click 1",
+      },
+    });
+  };
+  const clickHandle2 = () => {
+    TagManager.dataLayer({
+      dataLayer: {
+        event: "click_custom_button_2",
+        category: "button click 2",
+      },
+    });
+  };
+  const clickHandle = () => {
+    TagManager.dataLayer({
+      dataLayer: {
+        event: "click",
+        category: " click 2",
+      },
+    });
+  };
   return (
     <div className="App">
-      <button>GTM check</button>
+      <button onClick={() => clickHandle1()}>GTM check custom 1</button>
+      <button onClick={() => clickHandle2()}>GTM check custom 1</button>
+      <button onClick={() => clickHandle()}>click default</button>
     </div>
   );
 }
